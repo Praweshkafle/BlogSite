9726f01e52788d3e0d6497ef2aa65787d4ee6097
praweshkafle <75798623+Praweshkafle@users.noreply.github.com>
2023-11-07 21:45:53 +0545

    file manager added

---
+++ Core.Common/FileManager/IFileManager.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Http;
+
+namespace Core.Common.FileManager
+{
+    public interface IFileManager
+    {
+        bool isImageValid(string file_name);
+        string saveFileAndGetFileName(IFormFile file, string file_prefix = "");
+        string saveImageAndGetFileName(IFormFile file, string file_prefix = "");
+        bool isExcelFileValid(string file_name);
+        bool isImageSizeLessThan1Mb(IFormFile file);
+        string getRootPath();
+        string getPathToImageFolder();
+        string getPathToDatabaseFolder();
+        void deleteImage(string imageName, string path);
+    }
+}
