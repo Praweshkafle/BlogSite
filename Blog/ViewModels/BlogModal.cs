221741ba432b4eaa6a11fc2588b9222ef2dddbc3
praweshkafle <75798623+Praweshkafle@users.noreply.github.com>
2023-11-08 21:29:26 +0545

    vm created

---
+++ Blog/ViewModels/BlogModal.cs
@@ -0,0 +1,16 @@
+﻿namespace Blog.ViewModels
+{
+    public class BlogModal
+    {
+        public int Id { get; set; }
+
+        public string Title { get; set; }
+
+        public string Content { get; set; }
+
+        public DateTime PublicationDate { get; set; } = DateTime.Now;
+
+        public string AuthorName { get; set; }
+
+    }
+}
---
+++ Blog/ViewModels/HomeViewModel.cs
@@ -0,0 +1,14 @@
+﻿using Blog.Entities;
+using Blog.Services.Dto;
+
+namespace Blog.ViewModels
+{
+    public class HomeViewModel
+    {
+        public BlogPostDto BlogPostDto { get; set; }
+        public List<BlogPost> Posts { get; set; } = new List<BlogPost>(); 
+
+    }
+
+    
+}
