660da793aacc1180f702c806632c2ac58bd43562
praweshkafle <75798623+Praweshkafle@users.noreply.github.com>
2023-11-17 17:30:50 +0545

    my blog section added and edit option added to0

--- Blog/Controllers/BlogController.cs
+++ Blog/Controllers/BlogController.cs
@@ -59,7 +59,7 @@ namespace Blog.Controllers
                 AlertHelper.setMessage(this, ex.Message, messageType.error);
                 throw;
             }
-            return Json(new { success = true, message = "Failed To Post!" });
+            return Json(new { success = false, message = "Failed To Post!" });
         }
 
 
@@ -91,5 +91,78 @@ namespace Blog.Controllers
             }
             return View();
         }
+
+
+        [Route("edit/{Id}")]
+        [HttpGet]
+        public async Task<IActionResult> edit(int Id)
+        {
+            try
+            {
+                var user = await _userRepository.GetByIdAsync(getLoggedInUserId());
+                var blogDetail = await _blogPostRepository.GetByIdAsync(Id);
+                var blogModal = new BlogModal
+                {
+                    PublicationDate = blogDetail.PublicationDate,
+                    AuthorName = user.Username,
+                    Content = blogDetail.Content,
+                    Id = Id,
+                    Title = blogDetail.Title,
+                    Image = blogDetail.Image,
+                };
+                if (blogModal != null)
+                {
+                    return Json(new
+                    {
+                        Id = Id,
+                        PublicationDate = blogDetail.PublicationDate,
+                        AuthorName = user.Username,
+                        Content = blogDetail.Content,
+                        Title = blogDetail.Title,
+                        Image = blogDetail.Image,
+                    });
+                }
+            }
+            catch (Exception ex)
+            {
+                AlertHelper.setMessage(this, ex.Message, messageType.error);
+            }
+            return Json(null);
+        }
+
+        [Route("myblog")]
+        [HttpGet]
+        public async Task<IActionResult> myblog()
+        {
+            try
+            {
+                var blogs = await _blogPostRepository.GetUserPosts(getLoggedInUserId());
+                var blogPost = new List<BlogModal>();
+                foreach (var blog in blogs)
+                {
+                    var authname = await _userRepository.GetByIdAsync(blog.AuthorId);
+                    blogPost.Add(new BlogModal
+                    {
+                        Title = blog.Title,
+                        Content = blog.Content,
+                        PublicationDate = blog.PublicationDate,
+                        Id = blog.Id,
+                        AuthorName = authname.Username
+                    });
+                }
+                if (blogPost != null)
+                {
+                    return View(blogPost);
+                }
+                return View();
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+
+        }
+
+
     }
 }
