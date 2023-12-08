4afe6bd08bba5874d02a2cdcde7256936dddea57
praweshkafle <75798623+Praweshkafle@users.noreply.github.com>
2023-12-08 20:50:19 +0545

    comment model added

---
+++ Blog/ViewModels/CommentModel.cs
@@ -0,0 +1,16 @@
+﻿namespace Blog.ViewModels
+{
+    public class CommentModel
+    {
+        public int Id { get; set; }
+
+        public string Text { get; set; }
+
+        public DateTime CommentDate { get; set; }
+
+        public int AuthorId { get; set; }
+        public string Username { get; set; }
+        public bool isUser { get; set; } = false;
+        public int BlogPostId { get; set; }
+    }
+}
